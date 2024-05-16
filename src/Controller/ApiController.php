5245baeb5d11732cf9cc0fa6c1d1b1b26e97f1f0
Lucas Delobelle <lucas@ld-web.net>
2024-05-16 16:45:29 +0200

    :sparkles: v1

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,16 +4,34 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api', name: 'api_')]
 class ApiController extends AbstractController
 {
-    #[Route('/check', name: 'check')]
-    public function check(): JsonResponse
+    private const SPAM_DOMAINS = ['test.com', 'mailinator.com'];
+
+    #[Route('/check', name: 'check', methods: [Request::METHOD_POST])]
+    public function check(Request $request): JsonResponse
     {
+        $content = $request->toArray();
+
+        // clé email existe ? Email est valide ?
+
+        ['email' => $email] = $content;
+
+        $emailParts = explode('@', $email);
+        [, $domain] = $emailParts;
+
+        if (in_array($domain, self::SPAM_DOMAINS)) {
+            $result = 'spam';
+        } else {
+            $result = 'ok';
+        }
+
         return $this->json([
-            'message' => 'ok'
+            'result' => $result
         ]);
     }
 }
